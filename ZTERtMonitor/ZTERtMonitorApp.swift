362ee95e27aba5edfe07cbb9fc6b780efc1e9d85
nichiro <ichiro@nnip.org>
2023-01-24 20:31:08 +0000

    Initial Commit

---
+++ ZTERtMonitor/ZTERtMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZTERtMonitorApp.swift
+//  ZTERtMonitor
+//
+//  Created by 中橋 一朗 on 2023/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ZTERtMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
